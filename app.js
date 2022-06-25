18461bd6aa89a86ae29c4195866b39d5bf093c13
Abiola Ayodele <ayodelaustinab@gmail.com>
2022-06-25 08:48:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let myName = 'Abiola Ayodele';
 let height = '5.9"';
 let country = 'Nigeria';
 
-//This functions prints on screen
+//This functions prints on screen webpage
 function details(name, height, country) {
 	document.write(
 		`<h1>
@@ -12,6 +12,9 @@ function details(name, height, country) {
 	);
 }
 details(myName, height, country);
+
+//This function print on the screen as alert
+alert(`NAME: ${myName} \nHEIGHT: ${height} \nCOUNTRY: ${country}`);
 
 
 
